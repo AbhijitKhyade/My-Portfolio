ae8c77a37297fed830621a0538817d29640efa4e
AbhijitKhyade <abhijitkhyade@gmail.com>
2024-06-30 10:50:24 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,234 @@
+export const navItems = [
+  { name: "✦", link: "/" },
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  // { name: "Testimonials", link: "#testimonials" },
+  { name: "Work", link: "#work" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Exploring the world of web development",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm collaborative Team Player",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    // description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently exploring Next.js",
+    description: "Discovering Next.js for efficient web application development.",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Lets connect and build something amazing together!",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Resume Builder",
+    des: "Impressive application with sleek UI to build your resume in minutes.",
+    img: "/resume_builder.png",
+    iconLists: [
+      "/re.svg",
+      "/mui.png",
+      "/mongo.svg",
+      "/ex.svg",
+      "node.svg",
+
+    ],
+    link: "https://resume-builder-2024.vercel.app/",
+    liveorcode: true,
+  },
+  {
+    id: 2,
+    title: "District Scheme Tracker",
+    des: "Track the progress of various schemes in your district with ease.",
+    img: "",
+    iconLists: [
+      "/re.svg",
+      "/tail.svg",
+      "/mongo.svg",
+      "/ex.svg",
+      "node.svg",
+    ],
+    link: "",
+    liveorcode: true,
+  },
+  // {
+  //   id: 3,
+  //   title: "",
+  //   des: "Swap your skills, learn from the experienced",
+  //   img: "/SkillSwap.svg",
+  //   iconLists: [
+  //     "/re.svg",
+  //     "/tail.svg",
+  //     "/mongo.svg",
+  //     "/ex.svg",
+  //     "node.svg",
+
+  //     //  "/ts.svg", "/stream.svg", "/c.svg"
+  //   ],
+  //   link: "https://github.com/Wellitsabhi/Skillswap",
+  //   liveorcode: false,
+  // },
+  // {
+  //   id: 4,
+  //   title: "Todo",
+  //   des: "Manage all your daily tasks at one place, with progress tracker",
+  //   img: "/TODO.svg",
+  //   iconLists: ["/re.svg", "/tail.svg", "/mongo.svg", "/ex.svg", "node.svg"],
+  //   link: "https://wellitsabhi-todoapp.netlify.app/",
+  //   liveorcode: true,
+  // },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Full Stack Developer - NULLCLASS",
+    desc: "Deep dive in the development of a MERN stack project extending the functionality of Stack Overflow Website.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Full Stack Developer - Pune Institute of Computer Technology",
+    desc: "Developed a impressive web application leveraging the power of React.js and enhanced the user experience.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/AbhijitKhyade",
+  },
+  // {
+  //   id: 2,
+  //   img: "/twit.svg",
+  //   link: "https://x.com/wellitsabhi",
+  // },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/abhijit-khyade-954b9324b/",
+  },
+];
